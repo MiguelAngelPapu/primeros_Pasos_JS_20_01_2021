f54f1e2dc6662c6bce88929916c911e71a2f300c
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-01-20 19:32:30 -0500

    Tipos de datos

---
+++ main.js
@@ -0,0 +1,40 @@
+//  Tipos de datos 
+
+// string   cadena 
+
+"Miguel Angel"
+'Castro Escamilla'
+
+// number numero
+
+1000
+2,5
+-25
+
+//boolean afirmaciones
+true
+false
+
+
+//array listas
+
+['angel', 'fabian', 'andre']
+[1, 2 , 3, 4]
+[true, false, true, true]
+
+
+// object objeto
+
+{
+    nombre: "Miguel",
+    apellido: "Castro",
+    edad: 23,
+    profesor: true
+}
+
+{
+    nombre: "Diana",
+    apellido: "Bautista",
+    edad: 15,
+    profesor: null
+}
